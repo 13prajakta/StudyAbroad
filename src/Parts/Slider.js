996b6c22fd1372e23c04da5bd2a4dc89316312ab
13prajakta <13psathwane@gmail.com>
2021-06-10 13:30:26 +0530

    loc

--- src/Parts/Slider.js
+++ src/Parts/Slider.js
@@ -3,7 +3,7 @@ var Carousal1 = "3.jpg"
 function Slider() {
     return (
         <>
-            <div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
+            <div id="carouselExampleSlidesOnly" class="carousel slide slider" data-ride="carousel">
                 <div class="carousel-inner">
                     <div class="carousel-item active">
                         <img src={Carousal1} class="d-block w-100" alt="..." />
